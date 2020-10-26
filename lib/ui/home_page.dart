ddefc6d3d95a7543831b84c2fcb6065fe0100d62
Alysson Eduardo <alysson6801@hotmail.com>
2020-10-26 14:31:52 -0300

    (26/10) Cards de contato.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agendadecontatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -36,7 +38,44 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
           padding: EdgeInsets.all(10),
           itemCount: contacts.length,
-          itemBuilder: null
+          itemBuilder: (context, index){
+            return _contactCard(context, index);
+          }
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ? FileImage(File(contacts[index].img)) : AssetImage("images/person.png")
+                  )
+                )
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 10.00),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(contacts[index].name ?? "", style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)),
+                      Text(contacts[index].email ?? "", style: TextStyle(fontSize: 18.0)),
+                      Text(contacts[index].phone ?? "", style: TextStyle(fontSize: 18.0))
+                    ],
+                  ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
